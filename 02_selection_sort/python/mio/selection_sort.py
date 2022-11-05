6178dcd739ffee87cc58b758c190009738272482
Isidro Arias <isidroariass@hotmail.es>
2022-11-05 13:36:53 +0100

    selection sort experiments

--- 02_selection_sort/python/mio/selection_sort.py
+++ 02_selection_sort/python/mio/selection_sort.py
@@ -16,8 +16,25 @@ def selection_sort(unsorted):
         sort[i] = unsorted[smallest]
     return sort
 
+find_smallest = lambda l: l.index(min(l))
+
+def selection_sort(unsorted):
+    sort = []
+    for i in range(len(unsorted)):
+        smallest_index = find_smallest(unsorted)
+        smallest_value = unsorted.pop(smallest_index)
+        sort.append(smallest_value)
+    return sort
+
 
 def test_sort():
     unsorted = [5,3,6,2,10]
     sort = selection_sort(unsorted)
     assert sort == [2,3,5,6,10]
+
+def test_against_python_builtin():
+    from random import randint
+    unsorted = [randint(1,10**3) for i in range(10000)]
+    expected = sorted(unsorted)
+    sort = selection_sort(unsorted)
+    assert sort == expected
