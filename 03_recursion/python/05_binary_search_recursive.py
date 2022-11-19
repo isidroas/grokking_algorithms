a07a6ebfa7a59e75e01cd5768b79393b6e88fe5b
Isidro Arias <isidroariass@hotmail.es>
2022-11-19 13:17:09 +0100

    Demostration of bug in main

--- 03_recursion/python/05_binary_search_recursive.py
+++ 03_recursion/python/05_binary_search_recursive.py
@@ -13,3 +13,22 @@ def binary_search(arr, target):
         return binary_search(arr[:mid], target)
     else:
         return binary_search(arr[mid+1:], target)
+
+def test_bin():
+    assert binary_search([20],20) == 0
+    assert binary_search([20, 30],20) == 0
+    assert binary_search([20, 30],30) == 1
+    assert binary_search([20, 30],25) == None
+    assert binary_search([2, 3, 4],2) == 0
+
+    assert binary_search([1,5,10,20,50],1) == 0
+    assert binary_search([1,5,10,20,50],5) == 1
+    assert binary_search([1,5,10,20,50],10) == 2
+    assert binary_search([1,5,10,20,50],20) == 3
+    assert binary_search([1,5,10,20,50],50) == 4
+
+    assert binary_search([1,5,10,20],1) == 0
+    assert binary_search([1,5,10,20],5) == 1
+    assert binary_search([1,5,10,20],10) == 2
+    assert binary_search([1,5,10,20],20) == 3
+    assert binary_search([1,5,10,20],50) == None
