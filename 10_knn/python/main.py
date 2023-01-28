868475de0d49c7dd0aa686dacdf532c55ce4c407
Isidro Arias <isidroariass@hotmail.es>
2023-01-28 13:17:20 +0100

    working tests

--- 10_knn/python/main.py
+++ 10_knn/python/main.py
@@ -11,9 +11,9 @@ data = datasets.load_iris()
 
 X = np.array(data["data"])
 y = np.array(data["target"])
-classes = data["target_names"]
+CLASSES = data["target_names"]
 
-print(f"{classes=}")
+print(f"{CLASSES=}")
 print(f"{X[0]=}")
 print(f"{y=}")
 
@@ -53,8 +53,9 @@ def _get_mode(classes):
 def classify(X_train, y_train, X_test, k=5)-> int:
     dist = _distances(X_train, y_train, X_test)
     sort = _sort_dist(dist)
-    mode = _get_mode(sort[:k])
-    return mode
+    classes = (x[1] for x in sort[:k])
+    mode = _get_mode(classes)
+    return CLASSES[mode]
 
 def test_distances():
     assert _distances(X_train = [(2,2),(1,4)],y_train=(0,1), X_test = (1,2)) == [(1,0), (2,1)]
@@ -68,7 +69,9 @@ def test_mode():
 
 def test_classify():
     y_test_estimated = classify(X_train, y_train, [4.4, 3.1, 1.3, 1.4], k=5)
-    assert y_test_estimated == 'cetosa' # or 0
+    assert y_test_estimated == 'setosa' # or 0
+
+
 
 if __name__=="__main__":
     pass
