c4ed1cc42748ca716238fb47827612f4b828fe30
Isidro Arias <isidroariass@hotmail.es>
2023-02-05 11:23:59 +0100

    implemented deletion

--- 11_next/binary_search_tree.py
+++ 11_next/binary_search_tree.py
@@ -10,16 +10,53 @@ class Node:
 
 
 def search(tree, node):
+    if tree is None:
+        return False
+
     if tree.value == node:
         return True
 
     if node < tree.value:
-        return False if tree.left is None else search(tree.left, node)
+        return search(tree.left, node)
 
     if  node > tree.value:
-        return False if tree.right is None else search(tree.right, node)
+        return search(tree.right, node)
 
-def insert(tree,node):
+def delete(tree, node):
+    #if tree is None:
+    #    raise ValueError(f'Not found {node}')
+
+    #if tree.value == node:
+    #    #return True
+    #    tree.parent
+    #    del tree
+
+    if node < tree.value:
+        if tree.left is None:
+            raise ValueError(f'Not found {node}')
+        if tree.left.value == node:
+            if tree.left.left:
+                tree.left = tree.left.left
+            if tree.left.right:
+                insert_node(tree, tree.left.right)
+            del tree.left
+            return
+        return delete(tree.left, node)
+
+    if node > tree.value:
+        if tree.right is None:
+            raise ValueError(f'Not found {node}')
+        if tree.right.value == node:
+            if tree.right.left:
+                tree.right = tree.right.left
+            if tree.right.right:
+                insert_node(tree, tree.right.right)
+            del tree.right
+            return
+        return delete(tree.right, node)
+    assert False
+
+def insert(tree,node:str):
     if node < tree.value:
         if tree.left is None:
             tree.left = Node(node)
@@ -35,6 +72,22 @@ def insert(tree,node):
     if node == tree.value:
         raise ValueError(f'node {node} already exists')
 
+
+def insert_node(tree,node:Node):
+    if node.value < tree.value:
+        if tree.left is None:
+            tree.left = node
+            return
+        return insert(tree.left, node)
+
+    if node.value > tree.value:
+        if tree.right is None:
+            tree.right = node
+            return
+        return insert(tree.right, node)
+
+    if node.value == tree.value:
+        raise ValueError(f'node {node} already exists')
 
 def minimum(tree):
     res = tree.left
@@ -90,9 +143,12 @@ def test_tree():
     assert search(root, "Javier") == True
     assert search(root, "Manolo") == False
 
-    #insert(root,"Manolo")
-    #assert search(root,"Manolo") == True
+    insert(root,"Manolo")
+    assert search(root,"Manolo") == True
 
     assert minimum(root) == 'Alejandro'
     assert maximum(root) == 'Pedro'
-    assert traverse(root) == ['Alejandro', 'David','Javier', 'Jose','Juan', 'Miguel', 'Pedro']
+    assert traverse(root) == ['Alejandro', 'David','Javier', 'Jose','Juan', 'Manolo', 'Miguel', 'Pedro']
+
+    delete(root,"Manolo")
+    assert search(root,"Manolo") == False
