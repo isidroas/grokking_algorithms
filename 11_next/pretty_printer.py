f867f1d96208da34044e7abbb4ed127fba146cfb
Isidro Arias <isidroariass@hotmail.es>
2023-02-19 11:31:21 +0100

    proyect pretty printer

---
+++ 11_next/pretty_printer.py
@@ -0,0 +1,50 @@
+def list_of_lists():
+    return [[1,2],[3,4]]
+
+def dict_of_lists():
+    return {1:
+              [
+                  {2:[{4:[]}, {5:[]}]},
+                  {3:[{6:[]}, {7:[]}]},
+              ]
+           }
+
+def nodes():
+    return Node(1,
+              [
+                  Node(2,[Node(4), Node(5)]),
+                  Node(3,[Node(6), Node(7)]),
+              ])
+
+def test(list_):
+    assert pformat(list_) == r"""\
+    *
+   / \
+  *   *
+ / \ / \
+1  2 3  1
+"""
+
+def test(list_):
+    assert pformat(list_) == r"""\
+*
+│
+├── *
+│   ├── 1
+│   └── 2
+└── *
+    ├── 3
+    └── 4
+"""
+
+
+def test(list_):
+    assert pformat(list_) == r"""\
+       ┌─ 1
+  ┌─ * ┤
+  │    └─ 2
+* ┤
+  │    ┌─ 3
+  └─ * ┤
+       └─ 4\
+"""
